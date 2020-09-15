e7cc9331a765afacaacaa3d970e1b220063bd8f4
Rian Man <240743221@qq.com>
2020-09-15 15:49:15 +0800

    实现一个日历组件

---
+++ vue-example/index.js
@@ -0,0 +1,24 @@
+import app from './app.vue';
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Home from './page/home.vue';
+import Profile from './page/profile.vue';
+
+const routes = [
+    { path: '/', component: Home },
+    { path: '/profile', component: Profile }
+  ]
+
+const router = new VueRouter({
+    routes, // (缩写) 相当于 routes: routes,
+ 
+})
+
+Vue.use(VueRouter)
+new Vue({
+    // 挂载元素
+    el: '#app',
+    router: router,
+    render: h => h(app)
+})
+  
